a337084d39b0239b6c22c5bcc84d3e8e4b0a39aa
muhammadjon_tohirov <tokhirov.mukhammadjon@gmail.com>
2021-02-14 11:20:13 +0500

    Initial Commit

---
+++ TLibCore/TLibCore.h
@@ -0,0 +1,18 @@
+//
+//  TLibCore.h
+//  TLibCore
+//
+//  Created by Yuaufjon Tokhirov on 14/02/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TLibCore.
+FOUNDATION_EXPORT double TLibCoreVersionNumber;
+
+//! Project version string for TLibCore.
+FOUNDATION_EXPORT const unsigned char TLibCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TLibCore/PublicHeader.h>
+
+
